59cc4f94238c5e7fa531316dcde2ff6412006b37
dmarchand <dan.marchand@gmail.com>
2017-11-06 20:51:58 -0500

    Initial

---
+++ mm5rando/ConsoleApp1/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Megaman5Randomizer;
+
+namespace RandomizerConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Randomizer.RandomizeRom("foo");
+        }
+    }
+}
---
+++ mm5rando/Megaman5Randomizer/Randomizer.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Megaman5Randomizer
+{
+    public class Randomizer
+    {
+        public static void RandomizeRom(string path)
+        {
+
+        }
+    }
+}
