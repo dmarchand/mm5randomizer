1b76cefa15ca07a64273eead62a81b07f03fdf26
dmarchand <dan.marchand@gmail.com>
2017-11-06 22:54:29 -0500

    Output rom stuff

---
+++ mm5rando/ConsoleApp2/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using RomWriter;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            RomPatcher patcher = new RomPatcher();
+            patcher.AddRomModification(2960, 0x3A); // Should be first gravity man enemy
+            patcher.ApplyRomPatch("test.nes");
+        }
+    }
+}
--- mm5rando/RomWriter/RomPatcher.cs
+++ mm5rando/RomWriter/RomPatcher.cs
@@ -4,6 +4,7 @@ using System.Text;
 using RomWriter;
 using System.IO;
 
+
 namespace RomWriter
 {
     /// <summary>
