81823c9b9f3072cc0f4babd2f95114143619d9b1
Dan Marchand <dan.marchand@gmail.com>
2019-04-03 23:44:22 -0400

    Initial express stuff

--- mm5website/routes/index.js
+++ mm5website/routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/randomizer', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'MM5 Randomizer' });
 });
 
 module.exports = router;
